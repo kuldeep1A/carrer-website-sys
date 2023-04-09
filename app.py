e998b0c5f4ae4fc03aae10b0a7d7be71e8d60c43
Kuldeep1A <kuldeepdhangarkd@gmail.com>
2023-04-09 22:40:23 +0530

    abe update ho ja

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 JOBS = [
@@ -30,6 +30,10 @@ JOBS = [
 @app.route("/")
 def hello_k():
     return render_template("home.html", jobs=JOBS)
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
