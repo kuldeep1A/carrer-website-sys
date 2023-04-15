272942f9c3c9ceb1590ca31b3feec4c7cd98e371
Kuldeep1A <kuldeepdhangarkd@gmail.com>
2023-04-15 13:03:53 +0530

    chnages

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ def hello_k():
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs_list = load_jobs_from_db()
+    return jsonify(jobs_list)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
