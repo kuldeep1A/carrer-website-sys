df5e2599aea48a565bc04e62236e39bb1550f2a6
Kuldeep1A <kuldeepdhangarkd@gmail.com>
2023-04-15 14:15:03 +0530

    changes .gitignore

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 from flask import Flask, render_template, jsonify
+<<<<<<< HEAD
 from databases import load_jobs_from_db
 
 app = Flask(__name__)
 
+=======
+
+app = Flask(__name__)
+JOBS = [
+    {   
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {   
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Delhi , India',
+        'salary': 'Rs. 15,00,000'
+    },
+    {   
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote',
+        'salary': 'Rs. 12,00,000'
+    },
+    {   
+        'id': 5,
+        'title': 'Backend Engineer',
+        'location': 'San Francisco, USA',
+        'salary': '$ 120,000'
+    }
+]
+>>>>>>> parent of df78249 (add sqlalchemy to databases)
 @app.route("/")
 def hello_k():
-    jobs_list = load_jobs_from_db()
-    return render_template("home.html", jobs=jobs_list)
+    return render_template("home.html", jobs=JOBS)
 
 @app.route("/api/jobs")
 def list_jobs():
