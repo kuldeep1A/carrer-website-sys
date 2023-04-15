659a1c5900fb0bf60d1703c3a816e46f65def9e1
Kuldeep1A <kuldeepdhangarkd@gmail.com>
2023-04-15 14:15:03 +0530

    changes .gitignore

--- databases.py
+++ databases.py
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 from sqlalchemy import create_engine
 
-db_conn_string = "mysql+pymysql://pgi0lvar9ob7uq60gesa:pscale_pw_MSwMiGIPmiLzUcYMXOGuc6QYg3HEH6VMzmx8XKb7irv@aws.connect.psdb.cloud/career_website?charset=utf8mb4"
+
+=======
+from sqlalchemy import create_engine, text
+import os
+db_conn_string = os.environ.get["DB_CONNECTION_STRING"]
+>>>>>>> 18a1ed1 (chnages)
 
 engine = create_engine(
     db_conn_string,
@@ -19,4 +25,7 @@ def load_jobs_from_db():
             jobs.append(row._asdict())
         
     return jobs
+
+j = load_jobs_from_db()
+prnit(j)
 
