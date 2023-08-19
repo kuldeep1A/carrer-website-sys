341d8a241ec68b253e355ed7299e7e28ddd75776
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-08-19 23:50:04 +0530

    working

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, jsonify
-from databases import load_jobs_from_db
-
+from databases import  load_jobs_from_db
 app = Flask(__name__)
 
 @app.route("/")
 def hello_k():
-    return render_template("home.html", jobs=JOBS)
+    jobs_list = load_jobs_from_db()
+    return render_template("home.html", jobs=jobs_list)
+
 
 @app.route("/api/jobs")
 def list_jobs():
     jobs_list = load_jobs_from_db()
     return jsonify(jobs_list)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
